a2d1f6704ebaa6db8a9cbf60cf47f6a39f430baa
Misa <misa.kuroi.81@gmail.com>
2024-08-21 08:46:22 +0900

    ^^

--- script.js
+++ script.js
@@ -11,26 +11,3 @@ mask.addEventListener("click", function(){
 });
 
 
-
-// const swipeArea = document.querySelector('.swipe-area');
-// let startX;
-
-// swipeArea.addEventListener('touchstart', (e) => {
-//   startX = e.touches[0].clientX;
-// });
-
-// swipeArea.addEventListener('touchmove', (e) => {
-//   const moveX = startX - e.touches[0].clientX;
-//   swipeArea.scrollLeft += moveX;
-// });
-
-
-const swiper = new Swiper(".swiper", {
-    slidesPerView: 3,
-    breakpoints: {
-      // 768px以上の場合
-    //   768: {
-    //     slidesPerView: 3
-    //   }
-    }
-  });
